0dd275cb2964f77a810cbd4193f7b39a3bbf10f8
neel1998 <neelllovepainting@gmail.com>
2017-10-04 16:13:39 +0530

    commit

--- auto.sh
+++ auto.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-user=neel1998
-pswd=#abc123
+user="neel1998"
+pswd="#abc123"
 git add .
 git commit -m "$1"
 git push 
 expect "Username for 'https://www.github.com':"
-send "$user"
+send "$user\r"
 expect "password:"
 send "$pswd"
